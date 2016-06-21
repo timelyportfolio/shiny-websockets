58840fdc55b40d3bc16e37c0d6d40759fabbad3b
timelyportfolio <kent.russell@timelyportfolio.com>
2016-06-21 14:36:35 -0500

    make ui a function to accept data

--- shiny_react_json.R
+++ shiny_react_json.R
@@ -1,38 +1,40 @@
 library(htmltools)
 library(shiny)
 
-ui <- tagList(
-  tags$head(
-    tags$script(src="http://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react.min.js"),
-    #tags$script(src="http://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"),
-    tags$script(src="http://rawgit.com/arqex/react-json/master/build/Json.js"),
-    tags$link(href="http://rawgit.com/arqex/react-json/master/react-json.css",rel="stylesheet")
-  ),
-  tags$h1("react json editor"),
-  tags$script(HTML(
-    sprintf(
-'
-var doc = %s;
-/*
-// if you want to use JSX
-React.render(
-  <Json value={ doc } onChange={ logChange } />,
-  document.body
-);
-*/
-var Json;
-var app = React.createElement(Json, {value: doc, onChange: logChange});
-React.render(app, document.body);
-
-function logChange( value ){
-  console.log( value );
-  Shiny.onInputChange("reactjson", {value:value});
+ui_function <- function(json=NULL){
+  tagList(
+    tags$head(
+      tags$script(src="http://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react.min.js"),
+      #tags$script(src="http://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"),
+      tags$script(src="http://rawgit.com/arqex/react-json/master/build/Json.js"),
+      tags$link(href="http://rawgit.com/arqex/react-json/master/react-json.css",rel="stylesheet")
+    ),
+    tags$h1("react json editor"),
+    tags$script(HTML(
+      sprintf(
+  '
+  var doc = %s;
+  /*
+  // if you want to use JSX
+  React.render(
+    <Json value={ doc } onChange={ logChange } />,
+    document.body
+  );
+  */
+  var Json;
+  var app = React.createElement(Json, {value: doc, onChange: logChange});
+  React.render(app, document.body);
+  
+  function logChange( value ){
+    console.log( value );
+    Shiny.onInputChange("reactjson", {value:value});
+  }
+  '      ,
+        jsonlite::toJSON(json, auto_unbox=TRUE)
+      )
+    ))#,type="text/babel")
+  )
 }
-'      ,
-      jsonlite::toJSON(list(hola="amigo", array=1:3), auto_unbox=TRUE)
-    )
-  ))#,type="text/babel")
-)
 
 state <- list()
 
@@ -47,4 +49,7 @@ server <- function(input, output, session){
   )
 }
 
-runApp(list(ui=ui,server=server))
+runApp(list(
+  ui=ui_function(list(hola="amigo", array=1:3)),
+  server=server
+))
